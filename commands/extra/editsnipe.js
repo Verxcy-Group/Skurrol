1ea772b4eff73929532026e84ca225289c513ef0
byCRXHIT <70042525+byCRXHIT@users.noreply.github.com>
2022-02-04 08:33:09 +0100

    Update editsnipe.js

--- commands/extra/editsnipe.js
+++ commands/extra/editsnipe.js
@@ -7,13 +7,4 @@ module.exports = [{
     $onlyIf[$getChannelVar[esnipeOldMsg]!=undefinied;{description: there is nothing to snipe}{color: RED}]
     $onlyIf[$getChannelVar[msgEditorID]!=undefinied;{description: there is nothing to snipe}{color: RED}]
     $suppressErrors[There is nothing to snipe yet]`
-}, {
-  name: 'esnipe',
-  code: `$author[$username[$getChannelVar[msgEditorID]]#$discriminator[$getChannelVar[msgEditorID]];$userAvatar[$getChannelVar[msgEditorID]]]
-    $description[$getChannelVar[esnipeOldMsg]]
-    $addTimestamp
-    $color[RANDOM]
-    $onlyIf[$getChannelVar[esnipeOldMsg]!=undefinied;{description: there is nothing to snipe}{color: RED}]
-    $onlyIf[$getChannelVar[msgEditorID]!=undefinied;{description: there is nothing to snipe}{color: RED}]
-    $suppressErrors[There is nothing to snipe yet]`
 }]
