c510763271a780217352e20fed8638b1ea38c152
byCRXHIT <70042525+byCRXHIT@users.noreply.github.com>
2022-02-04 08:25:41 +0100

    Update eval.js

--- commands/admin/eval.js
+++ commands/admin/eval.js
@@ -1,6 +1,7 @@
 module.exports = {
     name: 'eval',
-    code: `$eval[$message]
-           $onlyForIDs[$getVar[owner];You're not allowed to use this command and tho' how'd you found it?]
-           `
+    code: `
+    $eval[$message]
+    $onlyForIDs[$getVar[owner];You're not allowed to use this command!]
+    `
 }
