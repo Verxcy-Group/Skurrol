3ef1c37cc6f258fc135fd54daa9ec45ecc1da0f4
byCRXHIT <70042525+byCRXHIT@users.noreply.github.com>
2022-02-04 08:32:25 +0100

    Update serverinfo.js

--- commands/extra/serverinfo.js
+++ commands/extra/serverinfo.js
@@ -1,3 +1,4 @@
+//INFO: This takes alot  power when sending, so be prepared for the bot to crash, or for it to take a while to send.
 module.exports = [{
     name: 'serverinfo',
     code: `$thumbnail[$serverIcon]
@@ -33,39 +34,5 @@ module.exports = [{
 
     🔨 **Bans:**
     $banCount Bans]`
-}, {
-    name: 'info',
-    code: `$thumbnail[$serverIcon]
-    $color[RANDOM]
-    $title[About The Server]
-    $description[
-    📝 **Name:** 
-    $serverName
-
-    😎 **Emojis:**
-    $serverEmojis
-
-    🌎 **Server Region:**
-    $serverRegion
-
-    👮 **Security Level:**
-    $serverVerificationLevel
-
-    🔥 **Channels:**
-    $channelCount[category] Categories
-    $channelCount[text] Text Channels
-    $channelCount[voice] Voice Channels
-
-    ⏫💖 **Boost Level:**
-    Level $serverBoostLevel
-
-    💖 **Boosts:**
-    $serverBoostCount Boosts
-
-    📊 **Members:**
-    $membersCount Members
-    $botCount Bots
-
-    🔨 **Bans:**
-    $banCount Bans]`
+}
 }]
