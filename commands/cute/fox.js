84682d7fdc4436c39bd6274f84d3f329723f2e39
byCRXHIT <70042525+byCRXHIT@users.noreply.github.com>
2022-02-06 14:30:02 +0100

    Update fox.js

--- commands/cute/fox.js
+++ commands/cute/fox.js
@@ -1,7 +1,9 @@
 module.exports = {
     name: 'fox',
-    code: `$title[$randomText[uwu :3;Uwu :3;uwU :3;uWu :3;UwU :3;UWU :3;uWu :3;uwu;Uwu;uwU;uWu;UWU]]
-           $image[$jsonRequest[https://randomfox.ca/floof/;image;Could not load a fox image! ( API Down or Rate limited )]]
-	   $cooldown[10s;Wait %time%. This cooldown is due to not get api rate limited.]
-           $color[RANDOM]`
+    code: `
+    $title[$randomText[uwu :3;Uwu :3;uwU :3;uWu :3;UwU :3;UWU :3;uWu :3;uwu;Uwu;uwU;uWu;UWU]]
+    $image[$jsonRequest[https://randomfox.ca/floof/;image;Could not load a fox image! ( API Down or Rate limited )]]
+    $cooldown[10s;Wait %time%. This cooldown is due to not get api rate limited.]
+    $color[RANDOM]
+    `
 }
