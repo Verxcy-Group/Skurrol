3959751ada5f5617ec8f715fa136f705a763fa81
byCRXHIT <70042525+byCRXHIT@users.noreply.github.com>
2022-02-06 14:11:47 +0100

    Update setMute.js

--- commands/mute/setMute.js
+++ commands/mute/setMute.js
@@ -1,15 +1,19 @@
 module.exports = [{
     name: 'setmute',
-    code: `$setServerVar[mute;$mentionedRoles[1]]
+    code: `
+    $setServerVar[mute;$mentionedRoles[1]]
     $title[Mute-role set!]
     $description[Succesfully set the Mute Role!
     Mute Role: <@&$mentionedRoles[1]>]
-    $color[GREEN]`
+    $color[GREEN]
+    `
 }, {
     name: 'set-mute',
-    code: `$setServerVar[mute;$mentionedRoles[1]]
+    code: `
+    $setServerVar[mute;$mentionedRoles[1]]
     $title[Mute-role set!]
     $description[Succesfully set the Mute Role!
     Mute Role: <@&$mentionedRoles[1]>]
-    $color[GREEN]`
+    $color[GREEN]
+    `
 }]
