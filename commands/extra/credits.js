0689f30f200199370ef2e9c7496f464d562c83d4
byCRXHIT <70042525+byCRXHIT@users.noreply.github.com>
2022-02-06 14:18:21 +0100

    Update credits.js

--- commands/extra/credits.js
+++ commands/extra/credits.js
@@ -1,106 +1,31 @@
 //Idk why but you can't add alises to command handlers so i just did this, sorry.
 module.exports = [{
   name: 'credits',
-  code: `$description[
+  code: `
+  $description[
         **Coding:**
         byCRXHIT
         AndreasKiller253
 
         **Api:**
-        [FakeMC Network](https://fakemc.ml)
-        [Music Function -> German](https://github.com/byCRXHIT/discord-musik-bot-DE)
+        [Verxcy Network](https://api.verxcydev.net)
         [MC Head Database](https://mc-heads.net)
+        [Website Status Checker](https://apivoid.com)
 
         **Art:**
         Dennis Dalinger
         
         **Ideas:**
         AToha1
+        Leon™️
 
         **Donators:**
         AndreasKiller253
 
         **Copyright:**
-        © 2021 byCRXHIT Software. All right reserved.
-        All images are self made and copyrighted.]
+        © 2022 Verxcy Development. All right reserved.
+        All images made by our Team and are not free to use.]
     $image[https://cdn.discordapp.com/attachments/808766425199804458/824618643522453564/Thanks-to.png]
-    $color[PURPLE]`
-}, {
-  name: 'thanks',
-  code: `$description[
-        **Coding:**
-        byCRXHIT
-        AndreasKiller253
-
-        **Api:**
-        [FakeMC Network](https://fakemc.ml)
-        [Music Function -> German](https://github.com/byCRXHIT/discord-musik-bot-DE)
-        [MC Head Database](https://mc-heads.net)
-
-        **Art:**
-        Dennis Dalinger
-        
-        **Ideas:**
-        AToha1
-
-        **Donators:**
-        AndreasKiller253
-
-        **Copyright:**
-        © 2021 byCRXHIT Software. All right reserved.
-        All images are self made and copyrighted.]
-    $image[https://cdn.discordapp.com/attachments/808766425199804458/824618643522453564/Thanks-to.png]
-    $color[PURPLE]`
-}, {
-  name: 'contributor',
-  code: `$description[
-        **Coding:**
-        byCRXHIT
-        AndreasKiller253
-
-        **Api:**
-        [FakeMC Network](https://fakemc.ml)
-        [Music Function -> German](https://github.com/byCRXHIT/discord-musik-bot-DE)
-        [MC Head Database](https://mc-heads.net)
-
-        **Art:**
-        Dennis Dalinger
-        
-        **Ideas:**
-        AToha1
-
-        **Donators:**
-        AndreasKiller253
-
-        **Copyright:**
-        © 2021 byCRXHIT Software. All right reserved.
-        All images are self made and copyrighted.]
-    $image[https://cdn.discordapp.com/attachments/808766425199804458/824618643522453564/Thanks-to.png]
-    $color[PURPLE]`
-}, {
-  name: 'contributors',
-  code: `$description[
-        **Coding:**
-        byCRXHIT
-        AndreasKiller253
-
-        **Api:**
-        [FakeMC Network](https://fakemc.ml)
-        [Music Function -> German](https://github.com/byCRXHIT/discord-musik-bot-DE)
-        [MC Head Database](https://mc-heads.net)
-
-        **Art:**
-        Dennis Dalinger
-        
-        **Ideas:**
-        AToha1
-
-        **Donators:**
-        AndreasKiller253
-
-        **Copyright:**
-        © 2021 byCRXHIT Software. All right reserved.
-        All images are self made and copyrighted.]
-    $image[https://cdn.discordapp.com/attachments/808766425199804458/824618643522453564/Thanks-to.png]
-    $color[PURPLE]`
-}]
+    $color[PURPLE]
+    `
+}
