93cffe25a4266b5af94662c1453e93cf8fd2b750
byCRXHIT <70042525+byCRXHIT@users.noreply.github.com>
2022-02-06 14:20:36 +0100

    Update base64.js

--- commands/extra/base64.js
+++ commands/extra/base64.js
@@ -1,13 +1,13 @@
 module.exports = {
     name: 'base64',
     code: `
-    $description[
-Your Message:
-$message
+        $description[
+            Your Message:
+            $message
 
-Base64 Encode:
-$jsonRequest[https://some-random-api.ml/base64?encode=$replaceText[$message; ;+];base64;Could not load the base64 decoder.]]
-$cooldown[5s;Wait %time%. This cooldown is due to not get api rate limited. Again.]
-$color[RANDOM]
-`
+            Base64 Encode:
+            $jsonRequest[https://some-random-api.ml/base64?encode=$replaceText[$message; ;+];base64;Could not load the base64 decoder.]]
+        $cooldown[5s;Wait %time%. This cooldown is due to not get api rate limited.]
+        $color[RANDOM]
+        `
 }
