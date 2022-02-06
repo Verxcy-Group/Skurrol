a092046f9e627aaf05b686f3bd06c53bb652b297
byCRXHIT <70042525+byCRXHIT@users.noreply.github.com>
2022-02-06 14:12:28 +0100

    Update bann.js

--- commands/moderation/bann.js
+++ commands/moderation/bann.js
@@ -1,15 +1,19 @@
 module.exports = [{
     name: 'ban',
-    code: `$title[User Banned]
+    code: `
+    $title[User Banned]
     $description[The User <@$mentioned[1]> got banned
 by: <@$authorID>]
     $ban[$mentioned[1]]
-    $argsCheck[1;Usage: +ban @user]`
+    $argsCheck[1;Usage: +ban @user]
+    `
 }, {
     name: 'bann',
-    code: `$title[User Banned]
+    code: `
+    $title[User Banned]
     $description[The User <@$mentioned[1]> got banned
 by: <@$authorID>]
     $ban[$mentioned[1]]
-    $argsCheck[1;Usage: +ban @user]`
+    $argsCheck[1;Usage: +ban @user]
+    `
 }]
