d23fc35de0b10fd331d47cb38971982e2c8db3da
byCRXHIT <70042525+byCRXHIT@users.noreply.github.com>
2022-02-06 14:13:11 +0100

    Update clear.js

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -6,5 +6,5 @@ module.exports = {
     $deleteIn[5s]
     $image[https://media.discordapp.net/attachments/808766425199804458/824606185889071124/Messages-Deleted.png]
     $clear[$message[1]]
-    $argsCheck[1;Error 1024, missing, wrong or too many arguments]`
+    $argsCheck[1;Usage: +clear <number>]`
 }
