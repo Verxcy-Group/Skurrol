ae2015e1f35aa081c2a7eab56412cc645619a9b7
byCRXHIT <70042525+byCRXHIT@users.noreply.github.com>
2022-05-07 13:58:29 +0200

    Updated to $getOwnerID

--- commands/admin/eval.js
+++ commands/admin/eval.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'eval',
     code: `
     $eval[$message]
-    $onlyForIDs[$getVar[owner];You're not allowed to use this command!]
+    $onlyForIDs[$botOwnerID;You're not allowed to use this command!]
     `
 }
